cacb657285d7f5cfc6b3e043f917111f3d107906
radziokoz06 <radziokoz08@gmail.com>
2022-07-27 13:09:10 +0200

    stateczki dalej

--- statki/main.py
+++ statki/main.py
@@ -1,5 +1,7 @@
+from msilib.schema import Error
 import random
-
+import os
+from tracemalloc import start
 
 letters = ["A","B","C","D","E","F","G","H","I","J"]
 numbers = ["1","2","3","4","5","6","7","8","9","10"]
@@ -7,14 +9,17 @@ pola = [['A1'], ['A2'], ['A3'], ['A4'], ['A5'], ['A6'], ['A7'], ['A8'], ['A9'], 
 for x in pola:
     x.append(False)
 def drukujPola():
+    nextRow = 2
     row = ""
     for x in pola:
-        if x[0] == "A10" or x[0] == "B10" or x[0] == "C10" or x[0] == "D10" or x[0] == "E10" or x[0] == "F10" or x[0] == "G10" or x[0] == "H10" or x[0] == "I10" or x[0] == "J10":
-            row += f"{x[0]} "
-            print(f"{row}  \n")
-            row = ""
+        if nextRow <=10:
+            row += f"{x[0]}  "
+            nextRow += 1
         else:
-            row += f"{x[0]} "
+            row += f"{x[0]}  \n"
+            print(row)
+            row = ""
+            nextRow = 2
 
 def losowaniePolozeniaStatkow():
     for x in range(24):
@@ -25,20 +30,47 @@ def losowaniePolozeniaStatkow():
             pola[polozenie].append(True)
         print(statek)
 
+losowaniePolozeniaStatkow()
+zestrzelone = []
 def startGry():
-    losowaniePolozeniaStatkow()
+    #os.system("cls")
     drukujPola()
-    while run:
-        strzal = input("W jakie pole chcesz strzelic? ")
-        for x in pola:
-            if x[0] == strzal:
-                if x[1] == True:
-                    print("Trafiony zatopiony!")
-                    x.remove(True)
-                    x.append(False)
-                else:
-                    print("Nie trafiony!")
-                  
+    strzal = input("W jakie pole chcesz strzelic? ")
+    czyStrzelano(strzal)
+    for x in pola:
+        if x[0] == strzal:
+            czyPole = True
+            if x[1] == True:
+                x.remove(True)
+                x.insert(1, False)
+                x.remove(strzal)
+                x.insert(0, "XX")
+                zestrzelone.append(strzal)
+                print(x)
+                startGry()
+            else:
+                czyPole = True
+                zestrzelone.append(strzal)
+                startGry()
+        else:
+            czyPole = False
+    if czyPole:
+            startGry()
+    else:
+        print("Wybierz poprawne pole!")
+        input()
+        startGry()
+def czyStrzelano(pole):
+    for x in pola:
+        czyPole = False
+        if x == pola[0]:
+            czyPole = True
+    if czyPole:
+        for x in zestrzelone:
+            if pole == x[0]:
+                return True
+        return False
+    else:
+        raise Error
 
-run = True
 startGry()
